2fde91a6c0289eabd5223ec328157c463bb0711c
Malmosmo <52857712+Malmosmo@users.noreply.github.com>
2021-11-24 11:41:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from setuptools import setup, find_packages
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-install_reqs = parse_requirements("requirements.txt", session=False)
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(
     name='msl',
     version='0.1.0',
@@ -19,6 +11,5 @@ setup(
     url='https://github.com/Malmosmo/MSL',
     license='LICENSE',
 
-    install_requires=reqs,
     long_description=open('README.md').read(),
 )
