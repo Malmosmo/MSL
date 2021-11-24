8b31565ac3612bac41ee682c2770bcefe27891dc
Malmosmo <52857712+Malmosmo@users.noreply.github.com>
2021-11-24 11:35:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='msl',
@@ -12,5 +15,6 @@ setup(
     url='https://github.com/Malmosmo/MSL',
     license='LICENSE',
 
+    install_requires=reqs,
     long_description=open('README.md').read(),
 )
